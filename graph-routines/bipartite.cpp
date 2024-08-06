5b6dc4768ec47424a0b451f18d10730fc542704a
XYLiu9357 <flotch2005@outlook.com>
2024-08-06 13:09:43 +0800

    changed bipartite routine naming

---
+++ graph-routines/bipartite.cpp
@@ -0,0 +1,7 @@
+/**bipartite.cpp
+ *
+ * A bipartite graph is a graph where all vertices can be divided into two disjoint sets
+ * such that no two graph vertices within the same set are adjacent.
+ *
+ * This routine returns the two partitions as iterable data types.
+ */
